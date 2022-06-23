507db6df25f566c1b8eb51990d9b66c2dcecf45c
Derek Colley <derek@colley.cc>
2022-06-23 11:22:36 +0100

    initial commit for api

---
+++ api/api/controllers/kusama/candidate.js
@@ -0,0 +1,42 @@
+const axios = require('axios')
+const moment = require('moment-timezone')
+
+const CACHE_URL = 'https://kusama.w3f.community/candidates'
+const CACHE_VALIDITY = 10 * 60 // 10 mins * 60 seconds
+const DATETIME_FORMAT = 'YYYY.MM.DD HH:mm:ss'
+
+var updatedAt = moment().add(-1, 'day')
+var cache = [
+  {}
+]
+
+module.exports = {
+  inputs: {
+    stash: {
+      type: 'string',
+      required: false
+    }
+  },
+  exits: {},
+  async fn (inputs, exits, env) {
+    if (moment().diff(updatedAt, 'seconds') > CACHE_VALIDITY) {
+      sails.log.debug('candidate.js: updating cache')
+      let result = await axios.get(CACHE_URL)
+      if (result.data) {
+        cache = result.data
+        updatedAt = moment()
+      }
+    } else {
+      sails.log.debug('candidate.js: serving from cache - ' + updatedAt.format(DATETIME_FORMAT))
+    }
+
+    if (inputs.stash) {
+      return exits.success({
+        updatedAt,
+        candidate: cache.find(f => f.stash === inputs.stash)
+      })
+    } else {
+      return exits.success({updatedAt, candidates: cache})
+    }
+  }
+}
