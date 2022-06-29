07aaecaf40afeee8527a767826503d22918407d7
Derek Colley <derek@colley.cc>
2022-06-29 15:21:51 +0100

    feature metapay

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -4,11 +4,11 @@
       <v-card-title>metaspan.io - tools</v-card-title>
       <v-card-text>
         <p>Here you will find various tools and utilities for blockchain-related applications.</p>
-        <p>The tools are open source, and the code can be found on github, <a href="https://github.com/metaspan">here</a></p>
+        <p>The tools are open source, and the code can be found on github, <a href="https://github.com/metaspan" target="_blank">here</a></p>
       </v-card-text>
     </v-card>
 
-    <v-card>
+    <v-card elevation=1>
       <v-card-title>Featured dApp: metaspan.io - kusama 1kv</v-card-title>
       <v-card-text>
         <p>Kusama 1KV (1000 validators)</p>
@@ -17,6 +17,17 @@
         </p>
       </v-card-text>
     </v-card>
+
+    <v-card elevation=1>
+      <v-card-title>Featured dApp: metaspan.io - met🌐pay</v-card-title>
+      <v-card-text>
+        <p>Pay with DOT or KSM</p>
+        <p>
+          <a href="https://metaspan.com/#metapay" target="_blank">met🌐pay</a>
+        </p>
+      </v-card-text>
+    </v-card>
+
   </v-container>
 </template>
 
